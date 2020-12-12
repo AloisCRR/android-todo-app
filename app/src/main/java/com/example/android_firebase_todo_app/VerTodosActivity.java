20896806ae87f3a5eb4e748dd2970f5ee16b86e6
Alois Carrera <aloiscarrera@gmail.com>
2020-12-12 17:32:40 -0500

    feat: :sparkles: Add list all TO-DO's operation (Implement)

---
+++ app/src/main/java/com/example/android_firebase_todo_app/VerTodosActivity.java
@@ -0,0 +1,13 @@
+package com.example.android_firebase_todo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerTodosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_todo);
+    }
+}
