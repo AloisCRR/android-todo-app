693bcad5823cdf5ee32d1fe29700f11b849c3c96
Alois Carrera <aloiscarrera@gmail.com>
2020-12-13 13:11:45 -0500

    chore: :truck: Rename recyclerAdapter to fix typo in name

--- app/src/main/java/com/example/android_firebase_todo_app/ToDoRecyclerAdapter.java
+++ app/src/main/java/com/example/android_firebase_todo_app/ToDoRecyclerAdapter.java
@@ -1,9 +1,11 @@
 package com.example.android_firebase_todo_app;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,16 +13,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.Timestamp;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class ToDoRecycleAdapter extends FirestoreRecyclerAdapter<TODO, ToDoRecycleAdapter.ToDoViewHolder> {
-    public ToDoRecycleAdapter(@NonNull FirestoreRecyclerOptions<TODO> options) {
+public class ToDoRecyclerAdapter extends FirestoreRecyclerAdapter<TODO, ToDoRecyclerAdapter.ToDoViewHolder> {
+
+    private static final String TAG = "PROBANDO";
+    ToDoListener toDoListener;
+
+    public ToDoRecyclerAdapter(@NonNull FirestoreRecyclerOptions<TODO> options, ToDoListener toDoListener) {
         super(options);
+        this.toDoListener = toDoListener;
     }
 
     @Override
@@ -28,7 +36,7 @@ public class ToDoRecycleAdapter extends FirestoreRecyclerAdapter<TODO, ToDoRecyc
         holder.titleTextView.setText(model.getTitle());
         holder.contentTextView.setText(model.getContent());
         holder.fechaTextView.setText(formatDate(model.getCreated().toDate()));
-        holder.checkBox.setChecked(model.getIsCompleted());
+        holder.checkBox.setChecked(model.getCompleted());
     }
 
     @NonNull
@@ -40,7 +48,7 @@ public class ToDoRecycleAdapter extends FirestoreRecyclerAdapter<TODO, ToDoRecyc
         return new ToDoViewHolder(view);
     }
 
-    public static class ToDoViewHolder extends RecyclerView.ViewHolder {
+    public class ToDoViewHolder extends RecyclerView.ViewHolder {
 
         TextView titleTextView, contentTextView, fechaTextView;
         CheckBox checkBox;
@@ -52,6 +60,26 @@ public class ToDoRecycleAdapter extends FirestoreRecyclerAdapter<TODO, ToDoRecyc
             contentTextView = itemView.findViewById(R.id.todoListContent);
             checkBox = itemView.findViewById(R.id.todoListCheckbox);
             fechaTextView = itemView.findViewById(R.id.todoListFecha);
+
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    DocumentSnapshot snapshot = getSnapshots().getSnapshot(getAdapterPosition());
+                    TODO todo = getItem(getAdapterPosition());
+                    if (todo.getCompleted() != isChecked) {
+                        toDoListener.handleCompleted(isChecked, snapshot);
+                    }
+                }
+            });
+        }
+
+        public void editTodo() {
+            toDoListener.handleEdit(getSnapshots().getSnapshot(getAdapterPosition()));
+            notifyItemChanged(getAdapterPosition());
+        }
+        
+        public void deleteToDo() {
+            toDoListener.handleDelete(getSnapshots().getSnapshot(getAdapterPosition()));
         }
     }
 
@@ -60,5 +88,11 @@ public class ToDoRecycleAdapter extends FirestoreRecyclerAdapter<TODO, ToDoRecyc
         DateFormat dateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
 
         return dateFormat.format(date);
+    }
+
+    interface ToDoListener {
+        void handleCompleted(boolean completed, DocumentSnapshot snapshot);
+        void handleEdit(DocumentSnapshot snapshot);
+        void handleDelete(DocumentSnapshot snapshot);
     }
 }
