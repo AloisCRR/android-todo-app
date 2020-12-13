d971eea91d4292ecf68443f36e096139528b4808
Alois Carrera <aloiscarrera@gmail.com>
2020-12-12 21:56:37 -0500

    refactor: :recycle: Add TO-DO class and onComplete method for short syntax

--- app/src/main/java/com/example/android_firebase_todo_app/CreateTodoActivity.java
+++ app/src/main/java/com/example/android_firebase_todo_app/CreateTodoActivity.java
@@ -3,20 +3,29 @@ package com.example.android_firebase_todo_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CreateTodoActivity extends AppCompatActivity {
@@ -35,38 +44,51 @@ public class CreateTodoActivity extends AppCompatActivity {
         txtTitle = (EditText) findViewById(R.id.editTextTituloTodo);
         txtContent = (EditText) findViewById(R.id.editTextContenidoTodo);
 
-        btnCrearTodo.setOnClickListener(v -> createTodo(v));
-        // verLista.setOnClickListener(v -> );
+        btnCrearTodo.setOnClickListener(this::createTodo);
+        btnVerLista.setOnClickListener(this::listaTodos);
     }
 
     public void createTodo(View v) {
 
-        if (TextUtils.isEmpty(txtTitle.getText().toString())||TextUtils.isEmpty(txtContent.getText().toString())) {
+        if (TextUtils.isEmpty(txtTitle.getText().toString()) || TextUtils.isEmpty(txtContent.getText().toString())) {
             Snackbar.make(v, "Debe ingresar texto en título y contenido", Snackbar.LENGTH_LONG).show();
             return;
         }
 
-        Map<String, Object> todoDocument = new HashMap<>();
+        txtTitle.onEditorAction(EditorInfo.IME_ACTION_DONE);
+        txtContent.onEditorAction(EditorInfo.IME_ACTION_DONE);
 
-        todoDocument.put("title", txtTitle.getText().toString());
-        todoDocument.put("content", txtContent.getText().toString());
-        todoDocument.put("completed", false);
-        todoDocument.put("created", Timestamp.now());
-        todoDocument.put("updated", Timestamp.now());
+        TODO todoDocument = new TODO(txtTitle.getText().toString(), txtContent.getText().toString(), false, Timestamp.now(), Timestamp.now());
 
-        firestore.collection("todos").add(todoDocument).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        firestore.collection("todos").add(todoDocument).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
             @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Snackbar.make(v, "¡TO DO creado con éxito!", Snackbar.LENGTH_LONG).show();
-                txtTitle.getText().clear();
-                txtContent.getText().clear();
-                txtTitle.requestFocus();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Snackbar.make(v, "¡Hubo un problema al crear el TO DO!", Snackbar.LENGTH_LONG).show();
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()) {
+                    Snackbar.make(v, "¡TO DO creado con éxito!", Snackbar.LENGTH_LONG).show();
+                    txtTitle.getText().clear();
+                    txtContent.getText().clear();
+                    txtTitle.requestFocus();
+                } else {
+                    Snackbar.make(v, "¡Hubo un problema al crear el TO DO!", Snackbar.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    public void listaTodos(View v) {
+        //        firestore.collection("todos").orderBy("created", Query.Direction.ASCENDING).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        //            private static final String TAG = "Probando";
+        //
+        //            @Override
+        //            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+        //                List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
+        //                for (DocumentSnapshot documentSnapshot : snapshotList) {
+        //                    Log.d(TAG, "onSuccess: " + documentSnapshot.getData());
+        //                }
+        //            }
+        //        });
+
+        Intent i = new Intent(this, VerTodosActivity.class);
+        startActivity(i);
     }
 }
